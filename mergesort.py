f7ed2263dc7d239347c439ce8d053d26c7a97bac
Nagaranjan-r-de <nagaranjan.r.de@gmail.com>
2026-09-23 12:06:01 +0530

    Add class Solution for duplicates and merge sort
    Implemented a class-based solution for removing duplicates and sorting a list using merge sort.

--- mergesort.py
+++ mergesort.py
@@ -1,25 +1,52 @@
-numbers = [5, 2, 8, 1, 3]
+class Solution(object):  
+    def duplicates(self,num):
+        corrected_list = []
+        
+        if len(num) < 1:
+            print("list is empty")
+                
+        for i in range (len(num)):
+            if num[i] not in corrected_list:
+                corrected_list.append(num[i])
+                
+        return self.divide(corrected_list)
 
-def divide(numbers):
+    def divide(self,divide_list):
+        
+        if len(divide_list) <= 1:
+            return divide_list
+        
+        mid = len(divide_list) // 2
+        
+        left = divide_list[:mid]
+        right = divide_list[mid:]
+        
+        left = self.divide(divide_list[:mid])
+        right = self.divide(divide_list[mid:])
+        
+        return self.merge(left, right)
 
-    if len(numbers) <= 1:
-        return numbers
-
-    mid = len(numbers) // 2
-
-    left = numbers[:mid]
-    right = numbers[mid:]
-
-    left = divide(left)
-    right = divide(right)
-
-    return left, right
-
-def merge(left, right):
-    merged = []
-    i = j = 0
-
+    def merge(self,left,right):#[7, 4, 5, 9, 2, 0, 3, 1]
+        
+        result = []
+        i, j = 0, 0
+        
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                result.append(left[i])
+                i += 1
+            else:
+                result.append(right[j])
+                j +=1
+                
+        result.extend(left[i:])
+        result.extend(right[j:])
+        
+        return result
+        
+        
     
+number = [7,4,5,9,2,7,5,9,0,4,3,2,1,3]
 
-divide(numbers)
-  
+sol = Solution()
+print(sol.duplicates(number))
